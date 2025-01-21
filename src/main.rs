41f508aa2a65183ab30638e598defed8b9f52d82
zhao-leo <18055219130@163.com>
2025-01-22 00:22:24 +0800

    移除未使用的库和函数，简化主函数逻辑

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 mod yolo;
 
-use std::{env, io::Write, process::exit};
-use reqwest::blocking::get;
-use std::fs::File;
-use zip::ZipArchive;
-use std::path::Path;
+use std::env;
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("初始化...");
-    dll_inject();
+
     // 从命令行获取模型路径
     let args: Vec<String> = env::args().collect();
     if args.len() < 4 {
@@ -26,38 +22,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     yolo::post_process(output,0.6,type_path)?;
     Ok(())
 }
-fn download_and_extract(url: &str, output_dir: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let response = get(url)?;
-    let bytes = response.bytes()?;
-    let mut file = File::create("libtorch.zip")?;
-    file.write_all(&bytes)?;
-    let file = File::open("libtorch.zip")?;
-    let mut archive = ZipArchive::new(file)?;
-    archive.extract(output_dir)?;
-    Ok(())
-}
-fn dll_inject() {
-    match env::var("LIBTORCH") {
-        Ok(path) => {
-            println!("LIBTORCH environment variable found in {}", path);
-        }
-        Err(_) => {
-            println!("No LIBTORCH environment variable found");
-            println!("Downloading libtorch cpu version...");
-            let download_url = "https://download.pytorch.org/libtorch/cpu/libtorch-win-shared-with-deps-2.5.1%2Bcpu.zip";
-            match download_and_extract(download_url, "./") {
-                Ok(_) => {
-                    println!("Downloaded and extracted libtorch successfully");
-                    env::set_var("LIBTORCH", Path::new("./libtorch").as_os_str());
-                    env::set_var("PATH", Path::new("./libtorch/lib").as_os_str());
-                    println!("LIBTORCH environment variable set to {:?}", Path::new("./libtorch").as_os_str());
-                    println!("PATH environment variable set to {:?}", Path::new("./libtorch/lib").as_os_str());
-                }
-                Err(e) => {
-                    println!("Failed to download and extract libtorch: {:?}", e);
-                    exit(1);
-                }
-            }
-        }
-    }
-}
+
