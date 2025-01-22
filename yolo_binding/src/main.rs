7b42b003b5ded3297120f98b6fad3eb9c2ad60d6
zhao-leo <18055219130@163.com>
2025-01-22 14:23:50 +0800

    更新 .gitignore，添加忽略文件；在 Cargo.toml 中添加新依赖；修改主函数以支持可选输出路径和置信度参数；重构后处理函数以保存输出图片并绘制预测框

--- yolo_binding/src/main.rs
+++ yolo_binding/src/main.rs
@@ -8,18 +8,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 从命令行获取模型路径
     let args: Vec<String> = env::args().collect();
     if args.len() < 4 {
-        println!("用法: {} <模型路径> <图片路径> <类型描述路径>", args[0]);
+        println!("用法: {} <模型路径> <图片路径> <类型描述路径> optional[-o <图片输出路径> -c <置信度>]", args[0]);
         return Ok(());
     }
     let model_path = &args[1];
     let image_path = &args[2];
     let type_path = &args[3];
-    println!("模型路径: {}  图片路径: {} ", model_path, image_path);
-    let image_tensor = yolo::preprocess_image(image_path)?;
+    let mut output_path = "output.jpg";
+    let mut confidence = 0.5;
+    for i in 4..args.len() {
+        if args[i] == "-o" {
+            output_path = &args[i + 1];
+        }
+        if args[i] == "-c" {
+            confidence = args[i + 1].trim().parse::<f64>().unwrap();
+        }
+    }
+    println!("模型路径: {}  图片路径: {} 输出路径: {} 置信度: {}", model_path, image_path, output_path, confidence);
+    
+    let image_tensor = yolo::preprocess_image(image_path)?;  //预处理图片
 
-    let output = yolo::predict(model_path, image_tensor)?;
+    let output = yolo::predict(model_path, image_tensor)?;  //模型预测
     println!("output: {:?}", output);
-    yolo::post_process(output,0.6,type_path)?;
+    yolo::post_process(output,confidence,type_path,image_path,output_path)?;
     Ok(())
 }
 
--- yolo_binding/src/yolo.rs
+++ yolo_binding/src/yolo.rs
@@ -1,6 +1,9 @@
+use image::{self, DynamicImage, GenericImageView, Rgba};
+use imageproc::drawing::{draw_line_segment_mut,draw_text_mut};
 use serde_json::Value;
 use std::{error::Error, fs, path::Path, time::Instant};
 use tch::{CModule, Device, Tensor};
+use ab_glyph;
 pub fn predict(model_path: &str, input: Tensor) -> Result<Tensor, Box<dyn Error>> {
     //Tensor should be on the same device as the model [1, 3, 640, 640]
     let device = Device::cuda_if_available();
@@ -23,7 +26,13 @@ pub fn preprocess_image(image_path: &str) -> Result<Tensor, Box<dyn Error>> {
     println!("Resized Image size: {:?}", image);
     Ok(image)
 }
-pub fn post_process(output: Tensor, conf: f64, type_path: &str) -> Result<(), Box<dyn Error>> {
+pub fn post_process(
+    output: Tensor,
+    conf: f64,
+    type_path: &str,
+    image_path: &str,
+    output_path: &str,
+) -> Result<(), Box<dyn Error>> {
     //Tensor [1, 84, 8400]
     let pred = output.transpose(2, 1).squeeze();
     let (npreds, pred_size) = pred.size2().unwrap();
@@ -33,13 +42,16 @@ pub fn post_process(output: Tensor, conf: f64, type_path: &str) -> Result<(), Bo
 
     let types: Value = serde_json::from_str(&json_buffer)?;
     println!("All types loaded");
-
+    let full_xywh = pred.slice(1, 0, 4, 1);
+    let mut picture = image::open(Path::new(image_path))?;
+    println!("Input picture: {}", image_path);
     for index in 0..npreds {
         // 遍历所有预测框
         let pred = pred.get(index);
         let mut max_conf = 0.0;
         let mut max_conf_index = 0;
         for i in 4..pred_size {
+            // 遍历所有类别
             if pred.double_value(&[i]) > max_conf {
                 max_conf = pred.double_value(&[i]);
                 max_conf_index = i;
@@ -48,8 +60,68 @@ pub fn post_process(output: Tensor, conf: f64, type_path: &str) -> Result<(), Bo
         if max_conf > conf {
             let class_index = max_conf_index - 4;
             let class_name = &types[class_index.to_string()];
-            println!("max_conf: {:?}, class_name: {}", max_conf, class_name);
+            print!("max_conf: {:.4}, class_name: {} ", max_conf, class_name);
+            image_process(&mut picture, full_xywh.slice(0, index, index + 1, 1),class_name.as_str().unwrap())?;
         }
     }
+    if let Ok(_) = picture.save(Path::new(output_path)){
+        println!("Output picture: {}", output_path);
+    }
+    Ok(())
+}
+
+fn image_process(picture: &mut DynamicImage, xywh: Tensor,type_name: &str) -> Result<(), Box<dyn Error>> {
+    //x,y,w,h is based on 640*640 image size
+    let (mut x, mut y, mut w, mut h) = (
+        xywh.double_value(&[0, 0]),
+        xywh.double_value(&[0, 1]),
+        xywh.double_value(&[0, 2]),
+        xywh.double_value(&[0, 3]),
+    );
+    println!("x: {:.2}, y: {:.2}, w: {:.2}, h: {:.2}", x, y, w, h);
+    (x, y, w, h) = (x / 640., y / 640., w / 640., h / 640.);  //Normalize to 1
+    let (width, height) = picture.dimensions();
+    (x, y, w, h) = (
+        x * width as f64,
+        y * height as f64,
+        w * width as f64,
+        h * height as f64,
+    );
+    let (x1, y1, x2, y2) = (x - w / 2., y - h / 2., x + w / 2., y + h / 2.);
+    draw_line_segment_mut(
+        picture,
+        (x1 as f32, y1 as f32),
+        (x2 as f32, y1 as f32),
+        Rgba([255, 0, 0, 255]),
+    );
+    draw_line_segment_mut(
+        picture,
+        (x2 as f32, y1 as f32),
+        (x2 as f32, y2 as f32),
+        Rgba([255, 0, 0, 255]),
+    );
+    draw_line_segment_mut(
+        picture,
+        (x2 as f32, y2 as f32),
+        (x1 as f32, y2 as f32),
+        Rgba([255, 0, 0, 255]),
+    );
+    draw_line_segment_mut(
+        picture,
+        (x1 as f32, y2 as f32),
+        (x1 as f32, y1 as f32),
+        Rgba([255, 0, 0, 255]),
+    );
+    let font = ab_glyph::FontRef::try_from_slice(include_bytes!("./arial.ttf"))?;
+    let scale = ab_glyph::PxScale::from(20.0);
+    draw_text_mut(
+        picture,
+        Rgba([255, 0, 0, 255]),
+        x1 as i32,
+        y1 as i32, // 调整文本位置
+        scale,
+        &font,
+        type_name,
+    );
     Ok(())
 }
